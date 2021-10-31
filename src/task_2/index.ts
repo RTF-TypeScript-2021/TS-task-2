9ba095f9280cc061bb1d127d8e451e49455d2b21
Angelina Shkatova <shkatova.angel@yandex.ru>
2021-10-31 23:51:25 +0500

    Task 2 is solved

--- src/task_2/index.ts
+++ src/task_2/index.ts
@@ -1,18 +1,18 @@
 /** Задача 1 - BankOffice
- * Имеется класс BankOffice. Который должен хранить пользователей и банковские карты.
+ * Имеется класс BankOffice, который должен хранить пользователей и банковские карты.
  * Пользователи банка могу иметь карту, а могут не иметь.
  * Карты могут иметь своего владельца, а могут не иметь.
  * Требуется:
  * 1) Реализовать классу BankOffice 3 метода:
- * 		1.1) authorize - позволяет авторизировать пользователя:
- * 			 Пользователь считается авторизованым, если карта принадлежит ему и пин-код введен корректно
- * 			 Принимает аргументы userId - id пользователя, cardId - id банковской карты, cardPin - пин-код карты
- * 			 Если пользователь был успешно авторизован, то метод возвращает true, иначе false
- * 		1.2) getCardById - позволяет получить объект банковской карты из хранилища по id карты
- *		1.3) isCardTiedToUser - позволяет по id карты узнать, привзяана ли карта к какому-нибудь пользователю
- *			 возвращает true - если карта привязана к какому-нибудь пользователю, false в ином случае
+ * 		1.1) authorize - позволяет авторизовать пользователя:
+ * 			 Пользователь считается авторизованным, если карта принадлежит ему и пин-код введен корректно.
+ * 			 Принимает аргументы userId - id пользователя, cardId - id банковской карты, cardPin - пин-код карты.
+ * 			 Если пользователь был успешно авторизован, то метод возвращает true, иначе false.
+ * 		1.2) getCardById - позволяет получить объект банковской карты из хранилища по id карты.
+ *		1.3) isCardTiedToUser - позволяет по id карты узнать, привязана ли карта к какому-нибудь пользователю
+ *			 возвращает true - если карта привязана к какому-нибудь пользователю, false в ином случае.
  * 2) Типизировать все свойства и методы класса MoneyRepository,
- * 	  пользуясь уже предоставленными интерфейсами (избавиться от всех any типов)
+ * 	  пользуясь уже предоставленными интерфейсами (избавиться от всех any типов).
 */
 
 import { Currency } from '../enums';
@@ -32,23 +32,28 @@ export interface IBankUser {
 }
 
 export class BankOffice {
-	private _users: any;
-	private _cards: any;
+	private _users: IBankUser[];
+	private _cards: ICard[];
 
-	constructor(users: any, cards: any) {
-		this._users = users;
-		this._cards = cards;
+	constructor(users: IBankUser[], cards: ICard[]) {
+	    this._users = users;
+	    this._cards = cards;
 	}
 
-	public authorize(userId: any, cardId: any, cardPin: any): any {
+	public authorize(userId: string, cardId: string, cardPin: string): boolean {
+		let user = this._users.find(user => user.id === userId);
+		let card = user.cards.find(card => card.id === cardId);
 
+		return user.cards.indexOf(card) !== -1 && card.pin === cardPin;
 	}
 
-	public getCardById(cardId: any): any {
-
+	public getCardById(cardId: string): ICard {
+		return this._cards.find(card => card.id === cardId);
 	}
 
-	public isCardTiedToUser(cardId: any): any {
-
+	public isCardTiedToUser(cardId: string): boolean {
+		return this._users.find(
+			user => user.cards.find(
+				card => card.id === cardId)) !== undefined;
 	}
 }
