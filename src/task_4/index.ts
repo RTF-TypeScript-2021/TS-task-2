e3f7dc7fe0f0d3f18adf6fd355b2026a0c5217c6
makar <misha.torno@gmail.com>
2021-10-31 18:23:03 +0500

    решил четветрый таск

--- src/task_4/index.ts
+++ src/task_4/index.ts
@@ -3,28 +3,36 @@
  * конвертацию валют.
  * Требуется:
  * 1) Реализовать классу CurrencyConverterModule 1 метод - convert
- * 	  метод должен принимать 3 аргумента:
- *		1.1) fromCurrency - валюта, из которой происходит конвертация
- *		1.2) toCurrency - валюта, в которую происходит конвертация
- *		1.3) moneyUnits - денежные единицы, полностью соответствующие валюте,
- *			 из которой происходит конвертация
- *	  Метод должен возвращать набор денежных единиц в той валюте, в которую происходит конвертация
- *	  Для простоты реализации будем считать, что банкомат конвертирует только по курсу
- *	  1USD = 70RUB и кратные курсу суммы (т.е. банкомат не может сконвертировать 100RUB, может только 70, 140 и т.д.)
+ *      метод должен принимать 3 аргумента:
+ *        1.1) fromCurrency - валюта, из которой происходит конвертация
+ *        1.2) toCurrency - валюта, в которую происходит конвертация
+ *        1.3) moneyUnits - денежные единицы, полностью соответствующие валюте,
+ *             из которой происходит конвертация
+ *      Метод должен возвращать набор денежных единиц в той валюте, в которую происходит конвертация
+ *      Для простоты реализации будем считать, что банкомат конвертирует только по курсу
+ *      1USD = 70RUB и кратные курсу суммы (т.е. банкомат не может сконвертировать 100RUB, может только 70, 140 и т.д.)
  * 2) Типизировать все свойства и методы класса UserSettingsModule,
- * 	  пользуясь уже предоставленными интерфейсами (избавиться от всех any типов)
-*/
+ *      пользуясь уже предоставленными интерфейсами (избавиться от всех any типов)
+ */
 
-import { Currency } from '../enums';
+import {Currency} from '../enums';
+import {IMoneyUnit, MoneyRepository} from "../task_1";
 
 export class CurrencyConverterModule {
-	private _moneyRepository: any;
+    private _moneyRepository: MoneyRepository;
 
-	constructor(initialMoneyRepository: any) {
-		this._moneyRepository = initialMoneyRepository;
-	}
+    constructor(initialMoneyRepository: MoneyRepository) {
+        this._moneyRepository = initialMoneyRepository;
+    }
 
-	public convertMoneyUnits(fromCurrency: Currency, toCurrency: Currency, moneyUnits: any): any {
+    public convertMoneyUnits(fromCurrency: Currency, toCurrency: Currency, moneyUnits: IMoneyUnit): number {
+        if (fromCurrency === toCurrency) {
+            return 0;
+        }
+        if (toCurrency === Currency.USD) {
+            return Number.parseInt(moneyUnits.moneyInfo.denomination) * moneyUnits.count / 70;
+        }
 
-	}
+        return Number.parseInt(moneyUnits.moneyInfo.denomination) * moneyUnits.count * 70;
+    }
 }
