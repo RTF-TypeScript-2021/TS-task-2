a28290c3b05607da30aa89db0c25f347a0ef4e19
Sergey <seregapax@gmail.com>
2021-11-16 17:50:07 +0500

    Add switch.

--- src/task_3/index.ts
+++ src/task_3/index.ts
@@ -3,73 +3,74 @@
  * изменение настроек пользователя.
  * Требуется:
  * 1) Реализовать классу UserSettingsModule 4 метода:
- * 		1.1) changeUserName - метод, заменяющий имя пользователя на переданное в аргументе
- * 			 возвращает true, если операция удалась и false в ином случае
- * 		1.2) changeUserSurname - метод, заменяющий фамилию пользователя на переданную в аргументе
- * 			 возвращает true, если операция удалась и false в ином случае
- * 		1.3) registerForUserNewCard - метод, привязывающий пользователю банковскую
- * 			 Карта считается успешно привязанной, если она существует и она не привязана ни к одному пользователю
- * 			 возвращает true, если операция удалась и false в ином случае
- * 		1.4) changeUserSettings - управляющий метод
- * 			 который возвращает результат работы одного из методов из 1.1 - 1.3
- * 			 на основе переданных аргументов
+ *        1.1) changeUserName - метод, заменяющий имя пользователя на переданное в аргументе
+ *             возвращает true, если операция удалась и false в ином случае
+ *        1.2) changeUserSurname - метод, заменяющий фамилию пользователя на переданную в аргументе
+ *             возвращает true, если операция удалась и false в ином случае
+ *        1.3) registerForUserNewCard - метод, привязывающий пользователю банковскую
+ *             Карта считается успешно привязанной, если она существует и она не привязана ни к одному пользователю
+ *             возвращает true, если операция удалась и false в ином случае
+ *        1.4) changeUserSettings - управляющий метод
+ *             который возвращает результат работы одного из методов из 1.1 - 1.3
+ *             на основе переданных аргументов
  * 2) Типизировать все свойства и методы класса UserSettingsModule,
- * 	  пользуясь уже предоставленными интерфейсами (избавиться от всех any типов)
-*/
+ *      пользуясь уже предоставленными интерфейсами (избавиться от всех any типов)
+ */
 
 import {UserSettingOptions} from '../enums';
 import {BankOffice, IBankUser, ICard} from "../task_2";
 
 export class UserSettingsModule {
-	private readonly _bankOffice: BankOffice;
-	private _user: IBankUser;
+    private readonly _bankOffice: BankOffice;
+    private _user: IBankUser;
 
-	public set user(user: IBankUser) {
-	    this._user = user;
-	}
+    public set user(user: IBankUser) {
+        this._user = user;
+    }
 
-	constructor(initialBankOffice: BankOffice) {
-	    this._bankOffice = initialBankOffice;
-	}
+    constructor(initialBankOffice: BankOffice) {
+        this._bankOffice = initialBankOffice;
+    }
 
-	private changeUserName(newName: string): boolean {
-	    if (!this._user || newName === this._user.name){
-	        return false
-	    }
-	    this._user.name = newName;
+    private changeUserName(newName: string): boolean {
+        if (!this._user || newName === this._user.name) {
+            return false
+        }
+        this._user.name = newName;
 
-	    return true;
-	}
+        return true;
+    }
 
-	private changeUserSurname(newSurname: string): boolean {
-	    if (!this._user || newSurname === this._user.surname){
-	        return false;
-	    }
-	    this._user.surname = newSurname;
+    private changeUserSurname(newSurname: string): boolean {
+        if (!this._user || newSurname === this._user.surname) {
+            return false;
+        }
+        this._user.surname = newSurname;
 
-	    return true;
-	}
+        return true;
+    }
 
-	private registerForUserNewCard(newCardId: string): boolean {
-	    const card: ICard = this._bankOffice.getCardById(newCardId);
+    private registerForUserNewCard(newCardId: string): boolean {
+        const card: ICard = this._bankOffice.getCardById(newCardId);
 
-	    if (this._bankOffice.isCardTiedToUser(newCardId) || !card){
-	        return false;
-	    }
-	    this._user.cards.push(card)
+        if (this._bankOffice.isCardTiedToUser(newCardId) || !card) {
+            return false;
+        }
+        this._user.cards.push(card)
 
-	    return true;
-	}
+        return true;
+    }
 
-	public changeUserSettings(option: UserSettingOptions, argsForChangeFunction: string): boolean {
-	    if (option === UserSettingOptions.name){
-	        return this.changeUserName(argsForChangeFunction);
-	    } else if (option === UserSettingOptions.surname){
-	        return this.changeUserSurname(argsForChangeFunction);
-	    } else if (option === UserSettingOptions.newCard){
-	        return this.registerForUserNewCard(argsForChangeFunction);
-	    }
-
-	    return false;
-	}
+    public changeUserSettings(option: UserSettingOptions, argsForChangeFunction: string): boolean {
+        switch (option) {
+        case UserSettingOptions.name:
+            return this.changeUserName(argsForChangeFunction);
+        case UserSettingOptions.surname:
+            return this.changeUserSurname(argsForChangeFunction);
+        case UserSettingOptions.newCard:
+            return this.registerForUserNewCard(argsForChangeFunction);
+        default:
+            return false;
+        }
+    }
 }
