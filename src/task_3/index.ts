49337a40331ac43e6f2341c9587d7c85945a12cb
echanatwell <koltunov.matwei@gmail.com>
2021-11-18 17:37:58 +0500

    Еще раз поправил :)

--- src/task_3/index.ts
+++ src/task_3/index.ts
@@ -17,7 +17,7 @@
  * 	  пользуясь уже предоставленными интерфейсами (избавиться от всех any типов)
 */
 
-import {UserSettingOptions} from '../enums';
+import {Currency, UserSettingOptions} from '../enums';
 import {BankOffice, IBankUser, ICard} from "../task_2";
 
 export class UserSettingsModule {
@@ -25,62 +25,67 @@ export class UserSettingsModule {
 	private _user: IBankUser;
 
 	public set user(user: IBankUser) {
-		this._user = user;
+	    this._user = user;
 	}
 
 	constructor(initialBankOffice: BankOffice) {
-		this._bankOffice = initialBankOffice;
+	    this._bankOffice = initialBankOffice;
 	}
 
 	private changeUserName(newName: string): boolean {
-		if (this._user === undefined) {
-			return false;
-		}
-		if (this._user.name === newName) {
-			return false;
-		}
-		this._user.name = newName;
+	    if (this._user === undefined) {
+	        return false;
+	    }
+	    if (this._user.name === newName) {
+	        return false;
+	    }
+	    this._user.name = newName;
 
-		return true;
+	    return true;
 	}
 
 	private changeUserSurname(newSurname: string): boolean {
-		if (this._user === undefined) {
-			return false;
-		}
-		if (this._user.surname === newSurname) {
-			return false;
-		}
-		this._user.surname = newSurname;
+	    if (this._user === undefined) {
+	        return false;
+	    }
+	    if (this._user.surname === newSurname) {
+	        return false;
+	    }
+	    this._user.surname = newSurname;
 
-		return true;
+	    return true;
 	}
 
 	private registerForUserNewCard(newCardId: string): boolean {
-		if (this._user === undefined) {
-			return false;
-		}
-		if (!this._bankOffice.isCardTiedToUser(newCardId)) {
-			let newCard: ICard;
-			newCard.id = newCardId;
-			this._user.cards.push(newCard);
+	    if (this._user === undefined) {
+	        return false;
+	    }
+	    if (!this._bankOffice.isCardTiedToUser(newCardId) &&
+			this._bankOffice.getCardById(newCardId)) {
+	        const newCard: ICard = {
+				id: newCardId,
+				balance: 0,
+				currency: Currency.RUB,
+				pin: "0000" };
+	        newCard.id = newCardId;
+	        this._user.cards.push(newCard);
 
-			return true;
-		} else {
-			return false;
-		}
+	        return true;
+	    } else {
+	        return false;
+	    }
 	}
 
 	public changeUserSettings(option: UserSettingOptions, argsForChangeFunction: string): boolean {
-		switch (option) {
-			case UserSettingOptions.name:
-				return this.changeUserName(argsForChangeFunction);
-			case UserSettingOptions.surname:
-				return this.changeUserSurname(argsForChangeFunction);
-			case UserSettingOptions.newCard:
-				return this.registerForUserNewCard(argsForChangeFunction);
-			default:
-				return false;
-		}
+	    switch (option) {
+	    case UserSettingOptions.name:
+	        return this.changeUserName(argsForChangeFunction);
+	    case UserSettingOptions.surname:
+	        return this.changeUserSurname(argsForChangeFunction);
+	    case UserSettingOptions.newCard:
+	        return this.registerForUserNewCard(argsForChangeFunction);
+	    default:
+	        return false;
+	    }
 	}
 }
