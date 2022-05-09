6437ab2987995dcee77d696138bc880d0ac4e344
6loodrain <89647718+6loodrain@users.noreply.github.com>
2022-05-09 19:18:14 +0500

    Update index.ts

--- src/task_4/index.ts
+++ src/task_4/index.ts
@@ -1,30 +1,76 @@
-/** Задача 4 - CurrencyConverterModule
- * Имеется класс CurrencyConverterModule. Который должен отвечать за
- * конвертацию валют.
+/** Задача 3 - UserSettingsModule
+ * Имеется класс UserSettingsModule. Который должен отвечать за
+ * изменение настроек пользователя.
  * Требуется:
- * 1) Реализовать классу CurrencyConverterModule 1 метод - convert
- * 	  метод должен принимать 3 аргумента:
- *		1.1) fromCurrency - валюта, из которой происходит конвертация
- *		1.2) toCurrency - валюта, в которую происходит конвертация
- *		1.3) moneyUnits - денежные единицы, полностью соответствующие валюте,
- *			 из которой происходит конвертация
- *	  Метод должен возвращать набор денежных единиц в той валюте, в которую происходит конвертация
- *	  Для простоты реализации будем считать, что банкомат конвертирует только по курсу
- *	  1USD = 70RUB и кратные курсу суммы (т.е. банкомат не может сконвертировать 100RUB, может только 70, 140 и т.д.)
+ * 1) Реализовать классу UserSettingsModule 4 метода:
+ * 		1.1) changeUserName - метод, заменяющий имя пользователя на переданное в аргументе
+ * 			 возвращает true, если операция удалась и false в ином случае
+ * 		1.2) changeUserSurname - метод, заменяющий фамилию пользователя на переданную в аргументе
+ * 			 возвращает true, если операция удалась и false в ином случае
+ * 		1.3) registerForUserNewCard - метод, привязывающий пользователю банковскую
+ * 			 Карта считается успешно привязанной, если она существует и она не привязана ни к одному пользователю
+ * 			 возвращает true, если операция удалась и false в ином случае
+ * 		1.4) changeUserSettings - управляющий метод
+ * 			 который возвращает резльтат работы одного из методов из 1.1 - 1.3
+ * 			 на основе переданных аргументов
  * 2) Типизировать все свойства и методы класса UserSettingsModule,
  * 	  пользуясь уже предоставленными интерфейсами (избавиться от всех any типов)
 */
 
-import { Currency } from '../enums';
+import { UserSettingOptions } from '../enums';
+import { IBankUser, BankOffice} from '../task_2';
 
-export class CurrencyConverterModule {
-	private _moneyRepository: any;
+export class UserSettingsModule {
+	private _bankOffice: BankOffice;
+	private _user: IBankUser;
 
-	constructor(initialMoneyRepository: any) {
-		this._moneyRepository = initialMoneyRepository;
+	public set user(user: IBankUser) {
+		this._user = user;
 	}
 
-	public convertMoneyUnits(fromCurrency: Currency, toCurrency: Currency, moneyUnits: any): any {
+	constructor(initialBankOffice: BankOffice) {
+		this._bankOffice = initialBankOffice;
+	}
 
+	private changeUserName(newName: string): boolean {
+		if (this._user === undefined){
+			return false;
+		}
+		this._user.name = newName;
+
+		return true;
+	}
+
+	private changeUserSurname(newSurname: string): boolean {
+		if (this._user.surname === newSurname){
+			return false;
+		}
+		this._user.surname = newSurname;
+
+		return true;
+	}
+
+	private registerForUserNewCard(newCardId: string): boolean {
+		if (this._bankOffice.isCardTiedToUser(newCardId)){
+			return false;
+		}
+		const card = this._bankOffice.getCardById(newCardId);
+		if (card === undefined){
+			return false;
+		}
+		this._user.cards.push(card);
+
+		return true;
+	}
+
+	public changeUserSettings(option: UserSettingOptions, argsForChangeFunction: string): boolean {
+		switch(option){
+			case UserSettingOptions.name:
+				return this.changeUserName(argsForChangeFunction);
+			case UserSettingOptions.surname:
+				return this.changeUserSurname(argsForChangeFunction);
+			case UserSettingOptions.newCard:
+				return this.registerForUserNewCard(argsForChangeFunction);
+		}
 	}
 }
